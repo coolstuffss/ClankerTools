a023258384633e07bb023cece157232cd9306cfb
Seri <serianoseri@gmail.com>
2020-03-08 04:34:24 +0100

    Lettura file/cartelle partendo da un Path in input

--- src/main.js
+++ src/main.js
@@ -1,23 +1,22 @@
 const { app, BrowserWindow } = require('electron')
-const customTitlebar = require('custom-electron-titlebar');
-  /*new customTitlebar.Titlebar({
-    backgroundColor: customTitlebar.Color.fromHex('#444')
-  });*/
-function createWindow () {
-  // Create the browser window.
+    /*new customTitlebar.Titlebar({
+      backgroundColor: customTitlebar.Color.fromHex('#444')
+    });*/
+function createWindow() {
+    // Create the browser window.
 
-  const win = new BrowserWindow({
-    width: 1000,
-    height: 1000,
-    webPreferences: {
-      nodeIntegration: true
-    }
-  })
+    const win = new BrowserWindow({
+        width: 1000,
+        height: 1000,
+        webPreferences: {
+            nodeIntegration: true
+        }
+    })
 
-  // and load the index.html of the app.
-  win.loadFile('index.html')
-  
-  win.maximize()
+    // and load the index.html of the app.
+    win.loadFile('index.html')
+
+    win.maximize()
 }
 
 // This method will be called when Electron has finished
@@ -27,19 +26,19 @@ app.whenReady().then(createWindow)
 
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
-  // On macOS it is common for applications and their menu bar
-  // to stay active until the user quits explicitly with Cmd + Q
-  if (process.platform !== 'darwin') {
-    app.quit()
-  }
+    // On macOS it is common for applications and their menu bar
+    // to stay active until the user quits explicitly with Cmd + Q
+    if (process.platform !== 'darwin') {
+        app.quit()
+    }
 })
 
 app.on('activate', () => {
-  // On macOS it's common to re-create a window in the app when the
-  // dock icon is clicked and there are no other windows open.
-  if (BrowserWindow.getAllWindows().length === 0) {
-    createWindow()
-  }
+    // On macOS it's common to re-create a window in the app when the
+    // dock icon is clicked and there are no other windows open.
+    if (BrowserWindow.getAllWindows().length === 0) {
+        createWindow()
+    }
 })
 
 // In this file you can include the rest of your app's specific main process
